f1f2308c6f6a0fb4c8b996fd60d55c7f0e20cf5e
swzhang1029 <msisutsu@hotmail.com>
2023-09-25 23:24:02 +1300

    sign in/up, connect with firebase

---
+++ app/src/main/java/com/example/groupassignment/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.groupassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        Typeface customFont = ResourcesCompat.getFont(this, R.font.nabila);
+        txtSlogan.setTypeface(customFont);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent signIn = new Intent(MainActivity.this, SignIn.class);
+                startActivity(signIn);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent signUp = new Intent(MainActivity.this, SignUp.class);
+                startActivity(signUp);
+            }
+        });
+    }
+}
